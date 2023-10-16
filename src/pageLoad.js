c1dff560e08f06f905c8c38c4dfdff80d9e8d2a7
danielkurtsawyer <19danielsawyer@gmail.com>
2023-10-16 16:10:49 -0400

    Finish adding comments to the pageLoad funtion

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -20,22 +20,27 @@ export default function pageLoad() {
     headerText.textContent = 'Thai Gourmet Food Truck';
     header.appendChild(headerText);
 
-    // add the header to the content div
+    // add the header to the general content div
     content.appendChild(header);
 
+    // create a container div to hold the main content of the page
     const main = document.createElement('div');
     main.classList.add('main');
 
+    // add the image of the food truck to the container
     const truckImage = new Image();
     truckImage.src = Truck;
     main.appendChild(truckImage);
 
+    // add the copy text to the container
     const copy = document.createElement('p');
     copy.textContent = 'This is my absolute favorite place to get quick, affordable, and delicious Thai food in Oakland. 5/5 stars! Would highly reccomend! - Dan';
     main.appendChild(copy);
 
+    // add the container div to the general content div
     content.appendChild(main);
 
+    // create the footer div and add relevent children elements
     const footer = document.createElement('div');
     footer.classList.add('footer');
     footer.textContent = 'Copyright 2023 Daniel Sawyer | ';
@@ -43,5 +48,6 @@ export default function pageLoad() {
     link.textContent = 'GitHub';
     link.setAttribute('href', 'https://github.com/danielkurtsawyer/odin-restaurant-page');
     footer.appendChild(link);
+    // add the footer to the general content div
     content.appendChild(footer);
 }
