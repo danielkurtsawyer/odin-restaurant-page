a9e4c41232feb0b979f806b83f362146f54779aa
danielkurtsawyer <19danielsawyer@gmail.com>
2023-10-16 11:28:17 -0400

    Integrate asset management with webpack
    I spent some time configuring webpack with the css and style loaders as well as the built in asset module.
    Now, webpack will handle distributing CSS styling, fonts, and images to the distribution folder.
    
    Additionally, I worked out some of the basic layout of the website.

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,15 +1,42 @@
+import Logo from './thai-gourmet-logo.png';
+import Truck from './thai-gourmet-food-truck.jpeg';
+
 export default function pageLoad() {
     const content = document.querySelector('#content');
-    const image = document.createElement('img');
-    image.setAttribute('src', '/src/thai-gourmet-food-truck.jpeg');
-    content.appendChild(image);
 
-    const headline = document.createElement('div');
-    headline.classList.add('header');
-    headline.textContent = 'Thai Gourmet Food Truck';
-    content.appendChild(headline);
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const logo = new Image();
+    logo.src = Logo;
+    logo.classList.add('logo');
+    header.appendChild(logo);
+    
+    const headerText = document.createElement('div');
+    headerText.textContent = 'Thai Gourmet Food Truck';
+
+    header.appendChild(headerText);
+    content.appendChild(header);
+
+    const main = document.createElement('div');
+    main.classList.add('main');
+
+    const truckImage = new Image();
+    truckImage.src = Truck;
+    main.appendChild(truckImage);
 
     const copy = document.createElement('p');
     copy.textContent = 'This is my absolute favorite place to get quick, affordable, and delicious Thai food in Oakland. 5/5 stars! Would highly reccomend! - Dan';
-    content.appendChild(copy);
+    main.appendChild(copy);
+
+    content.appendChild(main);
+
+    const footer = document.createElement('div');
+    footer.classList.add('footer');
+    footer.textContent = 'Copyright 2023 Daniel Sawyer | ';
+    const link = document.createElement('a');
+    link.textContent = 'GitHub';
+    link.setAttribute('href', 'https://github.com/danielkurtsawyer/odin-restaurant-page');
+    footer.appendChild(link);
+    content.appendChild(footer);
 }
